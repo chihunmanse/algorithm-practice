b4793580aed92025e526733c89c4cb2ea6c2ee9b
chihunmanse <chihunmanse@gmail.com>
2024-09-24 13:32:30 +0900

    mod : min_cost_climbing_stairs

---
+++ dp/min_cost_climbing_stairs/min_cost_climbing_stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        table = [- 1] * (len(cost) + 1)
+        table[0] = 0
+        table[1] = 0
+
+        for i in range(2, len(cost) + 1):
+            table[i] = min(table[i - 1] + cost[i - 1], table[i - 2] + cost[i -2])
+            
+        return table[len(cost)]
