e8e9ca91e6879c2a5d02fb267eabe18772ce9406
chihunmanse <chihunmanse@gmail.com>
2024-09-30 12:56:10 +0900

    add : path_with_maximum_probability.py

---
+++ heap/path_with_maximum_probability/path_with_maximum_probability.py
@@ -0,0 +1,31 @@
+import heapq
+
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        graph = defaultdict(list)
+
+        for i in range(len(edges)):
+            u, v = edges[i]
+            prob = succProb[i]
+            graph[u].append((prob, v))
+            graph[v].append((prob, u))
+        
+        probability = {i: 0 for i in range(n)}
+        probability[start_node] = 1
+
+        pq = [(-1, start_node)]
+
+        while pq:
+            cur_prob, cur_v = heapq.heappop(pq)
+            cur_prob = -cur_prob
+
+            if cur_v == end_node:
+                return cur_prob
+
+            for prob, next_v in graph[cur_v]:
+                next_prob = cur_prob * prob
+                if next_prob > probability[next_v]:
+                    probability[next_v] = next_prob
+                    heapq.heappush(pq, (-next_prob, next_v))
+        
+        return 0
